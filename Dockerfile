743b175d40056e206e9677e54674e7045b0ce23f
n-inanami <n-inanami@sakura.ad.jp>
2019-06-21 18:53:29 +0900

    default test parturn

--- Dockerfile
+++ Dockerfile
@@ -43,8 +43,22 @@ RUN cd ~/dev/trafficserver/tests \
  && env-test/bin/pip install pip --upgrade \
  && env-test/bin/pip install autest==1.7.2 hyper hyper requests dnslib httpbin traffic-replay
 
-# Run docker tests
+# Run trafficserver existing gold_tests
 COPY run-trafficserver-tests.sh /usr/local/bin/
-RUN /usr/local/bin/run-trafficserver-tests.sh || :
+
+USER root
+RUN chmod 777 /usr/local/bin/run-trafficserver-tests.sh
+
+USER build
+RUN /usr/local/bin/run-trafficserver-tests.sh
+
+# Run multiple cache control headers gold_tests
+COPY --chown=build:build tests/gold_tests/multiple-cache-control/ /tmp/multiple-cache-control/
+RUN mkdir ~/tmp
+RUN cd ~/dev/trafficserver/tests/gold_tests/ \
+ && mv * ~/tmp/ \
+ && mv ~/tmp/autest-site ~/dev/trafficserver/tests/gold_tests/
+RUN mv /tmp/multiple-cache-control ~/dev/trafficserver/tests/gold_tests/
+RUN /usr/local/bin/run-trafficserver-tests.sh
 
 ENTRYPOINT ["/bin/bash"]
---
+++ tests/gold_tests/multiple-cache-control/multi_cache_control.test.py
@@ -0,0 +1,61 @@
+'''
+parent proxy sometimes lost Cache-Control headers if origin response have two or more Cache-Control headers
+'''
+
+#  Licensed to the Apache Software Continueation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+
+Test.Summary = 'parent proxy sometimes lost Cache-Control headers if origin response have two or more Cache-Control headers'
+
+# Needs Curl
+Test.SkipUnless(
+    Condition.HasProgram("curl", "curl needs to be installed on system for this test to work"),
+)
+Test.ContinueOnFail = True
+
+# Define default ATS
+ts = Test.MakeATSProcess("ts")
+server = Test.MakeOriginServer("server")
+
+# Define test headers
+request_header1 = {"headers": "GET /default HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "12345678", "body": ""}
+response_header1 = {"headers": "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nConnection: close\r\n\r\n", "timestamp": "12345678", "body": "test"}
+server.addResponse("sessionlog.json", request_header1, response_header1)
+
+# ATS Configuration
+ts.Disk.plugin_config.AddLine('xdebug.so')
+ts.Disk.remap_config.AddLine(
+    'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
+)
+
+# Test 1 - 1 : not included Cache-Control ( 1st )
+tr = Test.AddTestRun()
+tr.Processes.Default.StartBefore(server)
+tr.Processes.Default.StartBefore(Test.Processes.ts, ready=1)
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/default'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/default_miss.gold"
+tr.StillRunningAfter = ts
+
+# Test 1 - 2 : not included Cache-Control ( 2nd )
+tr = Test.AddTestRun()
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/default'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/default_miss.gold"
+tr.StillRunningAfter = ts
+
