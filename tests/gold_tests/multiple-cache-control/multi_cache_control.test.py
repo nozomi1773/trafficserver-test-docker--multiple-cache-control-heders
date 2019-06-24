e068f3891e5dc321e732119c99d2ad303b985991
n-inanami <n-inanami@sakura.ad.jp>
2019-06-24 13:13:59 +0900

    add pattern

--- tests/gold_tests/multiple-cache-control/multi_cache_control.test.py
+++ tests/gold_tests/multiple-cache-control/multi_cache_control.test.py
@@ -38,6 +38,10 @@ request_header1 = {"headers": "GET /default HTTP/1.1\r\nHost: www.example.com\r\
 response_header1 = {"headers": "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nConnection: close\r\n\r\n", "timestamp": "12345678", "body": "test"}
 server.addResponse("sessionlog.json", request_header1, response_header1)
 
+request_header2 = {"headers": "GET /age HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "12345678", "body": ""}
+response_header2 = {"headers": "HTTP/1.1 200 OK\r\nContent-Length: 4\r\nConnection: close\r\nCache-Control: s-maxage=5\r\n\r\n", "timestamp": "12345678", "body": "test"}
+server.addResponse("sessionlog.json", request_header2, response_header2)
+
 ###### ATS Configuration ######
 # ATS Configuration ( child node )
 ts.Disk.plugin_config.AddLine('xdebug.so')
@@ -62,12 +66,14 @@ ts2.Disk.remap_config.AddLine(
     'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
 )
 ts2.Disk.records_config.update({
+   'proxy.config.http.insert_request_via_str' : 1,
+   'proxy.config.http.insert_response_via_str' : 3,
    'proxy.config.http.request_via_str' : 'ApacheTrafficServerParent',
    'proxy.config.http.response_via_str' : 'ApacheTrafficServerParent',
 })
 
 ###### Test Run ######
-# Test 1 - 1 : not included Cache-Control ( 1st )
+# Test 1 - 1 : not included Cache-Control ( 1st ) is cache miss
 tr = Test.AddTestRun()
 tr.Processes.Default.StartBefore(server)
 tr.Processes.Default.StartBefore(Test.Processes.ts)
@@ -77,10 +83,23 @@ tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/default_miss.gold"
 tr.StillRunningAfter = ts2
 
-# Test 1 - 2 : not included Cache-Control ( 2nd )
+# Test 1 - 2 : not included Cache-Control ( 2nd ) is cache miss
 tr = Test.AddTestRun()
 tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/default'.format(port=ts.Variables.port)
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/default_miss.gold"
 tr.StillRunningAfter = ts2
 
+# Test 2 -1 : included Cache-Control "Cache-Control: s-maxage: 5" ( 1st )
+tr = Test.AddTestRun()
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/age'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/age_miss.gold"
+tr.StillRunningAfter = ts2
+
+# Test 2 -2 : included Cache-Control "Cache-Control: s-maxage: 5" ( 2nd )
+tr = Test.AddTestRun()
+tr.Processes.Default.Command = 'curl -s -D - -v --ipv4 --http1.1 -H "x-debug: x-cache,via" -H "Host: www.example.com" http://localhost:{port}/age'.format(port=ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/age_hit.gold"
+tr.StillRunningAfter = ts2
